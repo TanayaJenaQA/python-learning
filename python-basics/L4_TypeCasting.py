d7d7d83fa459a7fde83c306e2583833cd79a4004
Tanay <tanaya.jenaqa@gmail.com>
2025-06-29 22:30:48 +0530

    Commits on python learning

--- python-basics/L4_TypeCasting.py
+++ python-basics/L4_TypeCasting.py
@@ -8,4 +8,7 @@ list_of_data= [1,2,3,4,5,6.9,6,6,6,9.7]
 set_of_data = set(list_of_data)
 print(set_of_data)
 
+print(bool(0), bool(1), bool(""))
+# we cant convert non numeric into numeric using type conversion concept
+
 # we can do same above vice versa
