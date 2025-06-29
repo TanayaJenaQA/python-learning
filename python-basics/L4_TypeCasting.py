28cce812fa2a176126d4ced91d0ac4dc433fe63a
Tanay <tanaya.jenaqa@gmail.com>
2025-06-29 21:38:40 +0530

    Commits on python learning

---
+++ python-basics/L4_TypeCasting.py
@@ -0,0 +1,11 @@
+num = 1
+print(type(num)) #<class 'int'>
+data = str(num)
+print(type(data)) #<class 'str'>
+print(float(num))  # int to float
+
+list_of_data= [1,2,3,4,5,6.9,6,6,6,9.7]
+set_of_data = set(list_of_data)
+print(set_of_data)
+
+# we can do same above vice versa
